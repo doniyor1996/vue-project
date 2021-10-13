e23f0403830b211fb7fb27a8418c3aa2ddd6b98f
Doniyor <doniyor199696@gmail.com>
2021-10-13 22:03:47 +0600

    counter

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,4 +2,31 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <div id="counter">Counter: {{ counter }}</div>
+  <button @click="stopTimer">Stop timer</button>
+  <button @click="startTimer">Start timer</button>
 </template>
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      counter: 0,
+      timer: null,
+    };
+  },
+  methods: {
+    stopTimer() {
+      clearInterval(this.timer);
+    },
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.counter++;
+      }, 1000);
+    },
+  },
+  mounted() {
+    this.startTimer();
+  },
+};
+</script>
