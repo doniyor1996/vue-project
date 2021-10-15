449f79a78d8017afef43e0c4ccfbae38fa877f9f
Doniyor <doniyor199696@gmail.com>
2021-10-15 21:08:17 +0600

    counter modern

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,23 +5,35 @@
   <div id="counter">Counter: {{ counter }}</div>
   <button @click="stopTimer">Stop timer</button>
   <button @click="startTimer">Start timer</button>
+  <br />
+  <br />
+  <div>My name is: {{ name }}</div>
+  <button @click="setMyName('Islom')">Islom</button>
+  <button @click="setMyName('Don')">Don</button>
 </template>
 <script>
 export default {
   name: "About",
   data() {
     return {
+      name: "Aziz",
+      names: ["Don", "Isa", "Az"],
       counter: 0,
       timer: null,
     };
   },
   methods: {
+    setMyName(newName) {
+      this.name = newName;
+    },
     stopTimer() {
       clearInterval(this.timer);
     },
     startTimer() {
       this.timer = setInterval(() => {
         this.counter++;
+        let index = this.counter % 3;
+        this.name = this.names[index];
       }, 1000);
     },
   },
