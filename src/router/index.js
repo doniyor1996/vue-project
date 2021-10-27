179640ddf4099c45997de6ee5604efa1d4b5cce7
MEGA MAX <islamisrailov0@gmail.com>
2021-10-27 20:46:57 +0600

    added element plus

--- src/router/index.js
+++ src/router/index.js
@@ -61,6 +61,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/MyComponents.vue"),
   },
+  {
+    path: "/element",
+    name: "Element-plus",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/element.vue"),
+  },
 ];
 
 const router = createRouter({
