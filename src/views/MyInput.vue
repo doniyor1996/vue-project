5b02c9e1d59981c8e8a4807e8a827bdfd4be43fa
Doniyor <doniyor199696@gmail.com>
2021-11-17 22:32:21 +0600

    refs

--- src/views/MyInput.vue
+++ src/views/MyInput.vue
@@ -1,7 +1,10 @@
 <template>
   <p>Input value: {{ value }}</p>
-  <input v-model="value" />
+  <input v-model="value" ref="textBox" @input="setColor('green')" />
   <button @click="clearValue">reset</button>
+  <div ref="history" class="values-history" style="border: 1px solid black">
+    prevValues:
+  </div>
 </template>
 <script>
 export default {
@@ -12,9 +15,22 @@ export default {
     };
   },
   methods: {
+    setColor(color) {
+      this.$refs.textBox.style.border = "1px solid " + color;
+      this.$refs.textBox.style.outline = "none";
+    },
     clearValue() {
+      this.setColor("red");
+      let p = document.createElement("p");
+      p.innerText = this.value;
+      this.$refs.history.append(p);
+      this.$refs.history.querySelector("p:first-child").style.color =
+        this.value;
       this.value = "";
     },
+  },
+  mounted() {
+    this.$refs.textBox.focus();
   },
 };
 </script>
